ed07ce263f1040266d679f88fc62e02861426283
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-04-07 13:54:49 +0530

    add intro with dp

--- ankit-kumar-portfolio/src/comps/Home.js
+++ ankit-kumar-portfolio/src/comps/Home.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import dp from '../imgs/dp.png'
+import Achivements from './Achivements'
+
 
 function Home() {
   return (
     <div className='main'>
       <div className="first-para">
-        <p id='hey'>
-          HEY !!!
-        </p>
         <p>
-          I'm <b>ANKIT KUMAR SINGH</b>. I"m a passionate long distance runner, dadicated to pushing my limits and achiveing my goals on road or trail. <br /> Running is not just a sport for me, it's a way of life that fuels my drive for excellence and Self improvement.
+          <p id='hey'>
+            HEY !!!
+          </p>
+          I'm <b>ANKIT KUMAR SINGH</b>. <br /> I"m a passionate long distance runner, dadicated to pushing my limits and achiveing my goals on road or trail. <br /> <br /> "Running is not just a sport for me, it's a way of life that fuels my drive for excellence and Self improvement."
         </p>
+        <img id='dp' src={dp} alt="dp" />
       </div>
+      <div className="second-para">
+        <p>I am a dedicated long - distance runner, demonstrating exceptional endurance & discipline in my athletic pursuits. My training regimen likely involves Consistent mileage, interval Sessions & cross  training to enhance performance and prevent injury. As an athlete, I prioritize proper nutrition, rest, and recovery to optimize my physical condition for races and Competitions. My Passion for running drives me Challenge myself, Setting personal goals & Striving for improvement With each race I undertake.</p>
+      </div>
+
+      <Achivements/>
     </div>
   )
 }
